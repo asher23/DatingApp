3996bfadb98c6fdf8342af677607ed300d6940b6
asher23 <sherabrar1@gmail.com>
2019-04-26 13:24:07 -0400

    End of Section 3

---
+++ DatingApp.API/Dtos/UserForLoginDto.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+  public class UserForLoginDto
+  {
+    public string username { get; set; }
+
+    public string password { get; set; }
+  }
+}
